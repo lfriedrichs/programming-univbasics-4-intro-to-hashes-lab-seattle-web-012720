775e819965dea069580cb681463b9e2456bbc83d
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-14 06:22:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,37 +4,37 @@ end
 
 def my_hash
   {
-    :name => "Dan"
+   # :name => "Dan"
   }
 end
 
 def pioneer
   {
-    :name => "Grace Hopper"
+   # :name => "Grace Hopper"
   }
 end
 
 def id_generator
   {
-    :id => 2
+   # :id => 2
   }
 end
 
 def my_hash_creator(key, value)
   {
-    key => value
+   # key => value
   }
 end
 
 def read_from_hash(hash, key)
-  hash[key]
+ # hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] {
-    hash[key] += 1
+ # if hash[key] {
+ #   hash[key] += 1
   }
-  else {
-    hash[key] = 1
+ # else {
+ #   hash[key] = 1
   }
 end
